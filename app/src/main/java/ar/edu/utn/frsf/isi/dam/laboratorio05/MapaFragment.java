b32b43253b3d82bb0da94816406a1058a13bddaf
martindestefanis <tinchodestefanis1@gmail.com>
2018-11-06 21:09:05 -0300

    LAB05 REQ01 Terminado

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
@@ -1,7 +1,10 @@
 package ar.edu.utn.frsf.isi.dam.laboratorio05;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,5 +40,18 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap map) {
         miMapa = map;
+        actualizarMapa();
     }
+    private void actualizarMapa() {
+        if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    9999);
+            return;
+        }
+        miMapa.setMyLocationEnabled(true);
+    }
+
 }
