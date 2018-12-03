edf85cf9d01a64a0336a1a60a9eea6ee98f3956b
Pedro Capdevila <pedro.capdevila@hotmail.com>
2018-12-03 11:50:03 -0300

    LAB 06 - REQ04 listos tests con Robolectric

--- app/src/test/java/ar/edu/utn/frsf/isi/dam/laboratorio05/NuevoReclamoFragmentRobolectricTest.java
+++ app/src/test/java/ar/edu/utn/frsf/isi/dam/laboratorio05/NuevoReclamoFragmentRobolectricTest.java
@@ -12,24 +12,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
 import ar.edu.utn.frsf.isi.dam.laboratorio05.modelo.Reclamo;
 
 import static org.junit.Assert.assertTrue;
-import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 @RunWith(RobolectricTestRunner.class)
 public class NuevoReclamoFragmentRobolectricTest {
-    /*private MainActivity activity;*/
+
     private NuevoReclamoFragment nuevoReclamoFragment;
 
     @Before
     public void setUp() throws Exception {
-        /*SupportFragmentController<NuevoReclamoFragment> controller = SupportFragmentController.of(nuevoReclamoFragment, MainActivity.class);
-        controller.create().resume().get();*/
-
         nuevoReclamoFragment = new NuevoReclamoFragment();
-        startFragment(nuevoReclamoFragment);
+        SupportFragmentTestUtil.startVisibleFragment(nuevoReclamoFragment);
     }
 
     @Test
@@ -62,5 +59,140 @@ public class NuevoReclamoFragmentRobolectricTest {
         imagen.setImageDrawable(null);
 
         assertTrue(!btnGuardar.isEnabled());
+    }
+
+    @Test
+    public void testNoHayDesc() throws Exception{
+        TextView tvCoord = (TextView) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_coord);
+        EditText reclamoDesc = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_desc);
+        EditText mail = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_mail);
+        Spinner tipoReclamo = (Spinner) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_tipo);
+        Button btnGuardar = (Button) nuevoReclamoFragment.getActivity().findViewById(R.id.btnGuardar);
+        ArrayAdapter<Reclamo.TipoReclamo> tipoReclamoAdapter;
+        tipoReclamoAdapter = new ArrayAdapter<Reclamo.TipoReclamo>(nuevoReclamoFragment.getActivity(), android.R.layout.simple_spinner_item, Reclamo.TipoReclamo.values());
+        tipoReclamoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoReclamo.setAdapter(tipoReclamoAdapter);
+
+        //SPINNER = SEMAFOROS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(2);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = ILUMINACION
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(3);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = RESIDUOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(5);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = RUIDOS_MOLESTOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(6);
+
+        assertTrue(!btnGuardar.isEnabled());
+    }
+
+    @Test
+    public void testDescMenorA8() throws Exception{
+        TextView tvCoord = (TextView) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_coord);
+        EditText reclamoDesc = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_desc);
+        EditText mail = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_mail);
+        Spinner tipoReclamo = (Spinner) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_tipo);
+        Button btnGuardar = (Button) nuevoReclamoFragment.getActivity().findViewById(R.id.btnGuardar);
+        ArrayAdapter<Reclamo.TipoReclamo> tipoReclamoAdapter;
+        tipoReclamoAdapter = new ArrayAdapter<Reclamo.TipoReclamo>(nuevoReclamoFragment.getActivity(), android.R.layout.simple_spinner_item, Reclamo.TipoReclamo.values());
+        tipoReclamoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoReclamo.setAdapter(tipoReclamoAdapter);
+
+        //SPINNER = SEMAFOROS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("desc");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(2);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = ILUMINACION
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("desc");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(3);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = RESIDUOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("desc");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(5);
+
+        assertTrue(!btnGuardar.isEnabled());
+
+        //SPINNER = RUIDOS_MOLESTOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("desc");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(6);
+
+        assertTrue(!btnGuardar.isEnabled());
+    }
+
+    @Test
+    public void testHayDesc() throws Exception{
+        TextView tvCoord = (TextView) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_coord);
+        EditText reclamoDesc = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_desc);
+        EditText mail = (EditText) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_mail);
+        Spinner tipoReclamo = (Spinner) nuevoReclamoFragment.getActivity().findViewById(R.id.reclamo_tipo);
+        Button btnGuardar = (Button) nuevoReclamoFragment.getActivity().findViewById(R.id.btnGuardar);
+        ArrayAdapter<Reclamo.TipoReclamo> tipoReclamoAdapter;
+        tipoReclamoAdapter = new ArrayAdapter<Reclamo.TipoReclamo>(nuevoReclamoFragment.getActivity(), android.R.layout.simple_spinner_item, Reclamo.TipoReclamo.values());
+        tipoReclamoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoReclamo.setAdapter(tipoReclamoAdapter);
+
+        //SPINNER = SEMAFOROS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("Descripción Reclamo");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(2);
+
+        assertTrue(btnGuardar.isEnabled());
+
+        /*//SPINNER = ILUMINACION
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("Descripción Reclamo");         ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ ESTE NO FUNCA HAY QUE VER QUE ONDA !!!!!!!!!!!!!!!!!!!!!
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(3);
+
+        assertTrue(btnGuardar.isEnabled());*/
+
+        //SPINNER = RESIDUOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("Descripción Reclamo");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(5);
+
+        assertTrue(btnGuardar.isEnabled());
+
+        //SPINNER = RUIDOS_MOLESTOS
+        tvCoord.setText("1;1");
+        reclamoDesc.setText("Descripción Reclamo");
+        mail.setText("dam@facu.com");
+        tipoReclamo.setSelection(6);
+
+        assertTrue(btnGuardar.isEnabled());
     }
 }
